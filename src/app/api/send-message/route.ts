988d7da0b80b9e63519091d6699e8f0d3cfd5386
Sunny Singh <sunnysingh706050@gmail.com>
2024-09-09 15:45:18 +0530

    implement send message logic

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,40 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import {Message} from "@/model/User";
+
+export async function POST(request: Request) {
+  // Connect to the database
+  await dbConnect();
+  const {username, content} = await request.json();
+  try {
+    const user = await UserModel.findOne({username}).exec();
+    if (!user) {
+      return Response.json(
+        {success: false, message: "User not found"},
+        {status: 404}
+      );
+    }
+    if (!user.isAcceptingMessages) {
+      return Response.json(
+        {success: false, message: "User is not accepting messages"},
+        {status: 403}
+      );
+    }
+
+    const newMessage = {content, createdAt: new Date()};
+    user.messages.push(newMessage as Message);
+    await user.save();
+    return Response.json(
+      {success: true, message: "Message sent successfully"},
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error("Error adding message:", error);
+    return Response.json(
+      {message: "Internal server error", success: false},
+      {status: 500}
+    );
+  }
+}
